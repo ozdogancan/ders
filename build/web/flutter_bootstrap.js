62663dcd2c5be2ffd5dc3349ea0ef3f4c1277578
ozdogancan <canozdogan@outlook.com>
2026-03-10 16:35:09 +0300

    fix: status bar renk ve onboarding mobile uyum

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"327ed814500baca0480a02f8555aa84a9afe32
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3531041681" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "569835951" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
