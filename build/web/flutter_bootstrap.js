4c76219e214c8064bcd48ec872343ac5b2271812
ozdogancan <canozdogan@outlook.com>
2026-03-13 09:07:45 +0300

    build: production release build

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"327ed814500baca0480a02f8555aa84a9afe32
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2653953004" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3835736410" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
